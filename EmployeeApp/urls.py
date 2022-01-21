27adb12d59010adb5ce7ba5321f9b1911f4fe84a
EC <cheng.eric11@gmail.com>
2022-01-21 17:47:45 -0500

    first commit

---
+++ EmployeeApp/urls.py
@@ -0,0 +1,18 @@
+from base64 import urlsafe_b64encode
+from django.urls import re_path
+from EmployeeApp import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+    re_path(r'^department$', views.departmentApi),
+    re_path(r'^department/([0-9]+)$', views.departmentApi),
+
+    re_path(r'^employees$', views.employeeApi),
+    re_path(r'^employees/([0-9]+)$', views.employeeApi),
+
+    re_path(r'^employees/savefile', views.SaveFile),
+
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
